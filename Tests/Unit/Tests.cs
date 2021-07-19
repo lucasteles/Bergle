bc588020ea780cf5989445f2ba8fff428ce014ae
Zaqueu <zaqueudovale@gmail.com>
2021-07-19 20:31:23 -0300

    Add Disciplina unit tests

--- Tests/Unit/Tests.cs
+++ Tests/Unit/Tests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Fadmin.Domain;
 using NUnit.Framework;
 
@@ -9,13 +12,107 @@ namespace Tests.Unit
         public void Setup() {}
 
         [Test]
-        public void StupidUnitTest()
+        public void NaoDeveAtualizarACargaHoraria()
         {
+            // Arrange
             var disciplina = new Disciplina();
 
-            disciplina.CargaHoraria = 50;
+            var novaCargaHoraria = 1;
 
-            Assert.AreEqual(50, disciplina.CargaHoraria);
+            // Act / Assert
+            Assert.Throws<Exception>(() => disciplina.AtalizarCargaHoraria(novaCargaHoraria));
+        }
+
+        [Test]
+        public void DeveAtualizarACargaHoraria()
+        {
+            // Arrange
+            var disciplina = new Disciplina();
+
+            var novaCargaHoraria = 42;
+
+            // Act
+            disciplina.AtalizarCargaHoraria(novaCargaHoraria);
+
+            // Assert
+            Assert.AreEqual(novaCargaHoraria, disciplina.CargaHoraria);
+        }
+
+        [Test]
+        public void DeveAdicionarUmNovoPreRequisito()
+        {
+            // Arrange
+            var disciplina = new Disciplina() { Id = 42 };
+
+            var preRequisito = new Disciplina() { Id = 41 };
+
+            // Act
+            disciplina.AdicionarPreRequisito(preRequisito);
+
+            // Assert
+            Assert.True(disciplina.PreRequisitos.Contains(preRequisito));
+        }
+
+        [Test]
+        public void UmaDisciplinaNaoPodeSerPrerequisitoParaSePropria()
+        {
+            // Arrange
+            var disciplina = new Disciplina() { Id = 42 };
+
+            // Act
+            disciplina.AdicionarPreRequisito(disciplina);
+
+            // Assert
+            Assert.True(!disciplina.PreRequisitos.Contains(disciplina));
+        }
+
+        [Test]
+        public void DeveAdicionarNovosPreRequisitos()
+        {
+            // Arrange
+            var disciplina = new Disciplina() { Id = 42 };
+
+            var preRequisitos = new List<Disciplina>()
+            {
+                new Disciplina() { Id = 10 },
+                new Disciplina() { Id = 20 },
+                new Disciplina() { Id = 30 }
+            };
+
+            // Act
+            disciplina.AdicionarPreRequisitos(preRequisitos);
+
+            // Assert
+            foreach (var preRequisito in preRequisitos)
+            {   
+                Assert.True(disciplina.PreRequisitos.Contains(preRequisito));
+            }
+        }
+
+        [Test]
+        public void NaoDeveDuplicarUmPreRequisito()
+        {
+            // Arrange
+            var disciplina = new Disciplina() { Id = 42 };
+
+            var preRequisitos = new List<Disciplina>()
+            {
+                new Disciplina() { Id = 10 },
+                new Disciplina() { Id = 20 },
+                new Disciplina() { Id = 30 }
+            };
+
+            disciplina.AdicionarPreRequisitos(preRequisitos);
+
+            // Act
+            disciplina.AdicionarPreRequisito(preRequisitos.First());
+
+            // Assert
+            foreach (var preRequisito in preRequisitos)
+            {   
+                Assert.True(disciplina.PreRequisitos.Contains(preRequisito));
+            }
+            Assert.AreEqual(preRequisitos.Count(), disciplina.PreRequisitos.Count());
         }
     }
 }
